65bfb57031dd9f3de41d851310fda0aa719af4ca
Corey <17171838+coreyryanhanson@users.noreply.github.com>
2020-09-11 17:52:20 -0400

    aggregating noaa data complete will need another source for rainfall

--- functions/cleaning_functions.py
+++ functions/cleaning_functions.py
@@ -1,8 +1,43 @@
 import os
 import re
+import math
 import gzip
 import numpy as np
 import pandas as pd
+
+
+def pandas_series_autocheck(df, column, index, default_value=np.nan):
+    if column in df.columns:
+        return df[column]
+    else:
+        return pd.Series(default_value, index=index)
+
+
+def degrees_to_cos(x):
+    """Takes the cosine of an angle given in degrees."""
+    return math.cos(math.radians(x))
+
+
+def degrees_to_sin(x):
+    """Takes the sine of an angle given in degrees."""
+    return math.sin(math.radians(x))
+
+
+def convert_to_wind_velocity(data, angle_col, speed_col, new_col_base = "Wind_Vel", drop_speed=True, drop_angle=True):
+    """Takes a dataframe and the column names for angle and speed to break them into their x and y component vectors."""
+    
+    df = data.copy()
+    
+    # Creates new columns using the sine and cosine function.
+    for suffix, function in {"_x":degrees_to_cos, "_y":degrees_to_sin}.items():
+        df[new_col_base+suffix] = df[angle_col].map(function) * df[speed_col]
+    
+    # Drops original columns depending on function arguments.
+    for column, drop in {angle_col:drop_angle, speed_col:drop_speed}.items():
+        if drop:
+            df.drop(columns=column, inplace=True)
+    return df
+
 
 def standardize_CMC_tidal_strings(value):
     if type(value) is not str:
@@ -136,3 +171,83 @@ def reduce_time_resolution_in_multindex(data, timekey, freq, other_keys):
     
     
     return df.groupby([pd.Grouper(key=timekey, freq=freq), *other_keys]).agg(agg_list).dropna(how="all").reset_index()
+
+
+def accumulate_noaa_observations(data, date_column, period_column, value_column, id_columns, cycles, available_period, time_unit="H"):
+    """This function will examine a dataframe with mixed period data and cycle through summing up earlier values in that period if they
+    exist. There is a tradeoff with speed, precision, and sparcity and this function rounds the time values to achieve its
+    goal as quickly as possible. If any value is incomplete over the interval, it will be considered a nan in the final sum."""
+    
+
+    # Saves the original index and creates a copy of the dataframe where index values are reset to prevent side effects
+    # and offset bugs being introduced during the merge.
+    idx = data.index
+    df = data.reset_index().copy()
+    
+    # Restrict values to ones that match the specified period.
+    df[value_column] = np.where(df[period_column]==str(available_period), df[value_column], np.nan)
+    
+    # Round to hours to prevent differences in minutes hindering the search for subsequent periods.
+    df[date_column] = df[date_column].round(time_unit)
+    
+    # These columns will contain the list of unique identifiers, dates, and period to execute the joins.
+    relevant_columns = [*id_columns, date_column, period_column]
+    
+    # Creates the reference that will be joined on the offset time values.
+    reference = df[relevant_columns+[value_column]]
+    
+    # The values will be aggregated in this defensive copy.
+    aggregate = reference.copy()
+    
+    # The reference needs to remove duplicate rows for a true 1:1 join.
+    reference = reference.groupby(relevant_columns).first()
+    
+    # ITerates through the cycles of the provided time, offsetting the dates and joining available values from earlier observations
+    # on those offsetted indexes.
+    for c in np.arange(1, cycles):
+        aggregate[date_column] = df[date_column] - pd.Timedelta(c * available_period, unit=time_unit)
+        aggregate = aggregate.merge(reference, how="left", on=relevant_columns, suffixes=(None, c))
+    
+    # Ensures that the only values included in the sum are observations.
+    aggregate.drop(columns=relevant_columns, inplace=True)
+    
+    # Returns the index back to its original value.
+    aggregate.index = idx
+    
+    # Returns the sum of the columns. If any value is missing, it will be a nan.
+    return aggregate.sum(axis=1, skipna=False)
+
+
+def aggregate_noaa_quantity(data, output_column, date_column, period_column, value_column, id_columns,
+                            instances, desired_period, available_period, time_unit="H"):
+    """This function will parse NOAA variable periods in specified columns of a NOAA DataFrame and save them to their own distinct
+    column, aggregating values from past data values if needed."""
+    
+    # Creates a defensive copy of the DataFrame and backs up the index.
+    df = data.copy()
+    idx = df.index
+    
+    # User created function to check whether a column exists in the DataFrame. If not a new one is populated by nans.
+    df[output_column] = pandas_series_autocheck(df, output_column, idx)
+    
+    # Periods must be cleanly divisible or results will be inaccurate.
+    if desired_period % available_period:
+        raise ValueError("Warning time periods are out of sync and will result in incorrect values.")
+    cycles = math.ceil(desired_period/available_period)
+    
+    for i in np.arange(1,instances+1):
+        
+        # This if statement will save computation time if the period column is not greater than the available period. Replacement
+        # values are either taken as is with a perfect match or the sums are calculated with a user driven function offsetting
+        # based on the amount of cycles required and conservatively adding if observations exist in all time offsets.
+        if cycles == 1:
+            replacement_values = df[value_column+str(i)]
+        else:
+            replacement_values = accumulate_noaa_observations(df, date_column, period_column+str(i), value_column+str(i),
+                                                              id_columns, cycles, available_period)
+        
+        # Requires new values to be only added when existing ones are not available, and the period is a correct match.
+        condition = (df[output_column].isna()) & (df[period_column+str(i)] == str(available_period))
+        df[output_column] = np.where(condition, replacement_values, df[output_column])
+            
+    return df
